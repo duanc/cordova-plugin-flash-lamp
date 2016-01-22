faead7ccfd0aad4d9f2644c7435e9a3ff012e868
duanc <duan22334@163.com>
2016-01-22 15:58:01 +0800

    no message

--- src/android/FlashLampPlugin.java
+++ src/android/FlashLampPlugin.java
@@ -17,7 +17,7 @@ public class FlashLampPlugin extends CordovaPlugin {
     private CallbackContext callbackContext;
 
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
 
 //        this.callbackContext = callbackContext;
 //        Toast.makeText(this.cordova.getActivity(),action,LENGTH_SHORT).show();
